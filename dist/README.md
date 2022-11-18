ae44580cb254f9d84f8205d315b7cdf25bc9fa5d
emanuelm1 <emanuelm52001@gmail.com>
2022-11-18 07:10:01 -0800

    Add files via upload

---
+++ dist/README.md
@@ -0,0 +1,41 @@
+
+<h1 align="center">test 👋</h1>
+  
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+
+## Description
+🔍 a test
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 download
+
+## Usage
+💻 for a test
+
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+<br />
+This application is covered by the MIT license. 
+
+## Contributing
+👪 you
+
+## Tests
+✏️ sure
+
+## Questions
+✋ troubleshoot<br />
+<br />
+:octocat: Find me on GitHub: [emanuelm01](https://github.com/emanuelm01)<br />
+<br />
+✉️ Email me with any questions: test@testmail.com<br /><br />
+    
---
+++ index.js
@@ -0,0 +1,87 @@
+// Declaring the dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe install process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Project usage?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What license was used?: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Any contributors?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Test to add?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do for issues? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Enter GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
